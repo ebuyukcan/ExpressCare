77f15c5f0d88b9af1cef6fc9e354679318710bf9
Joel Mertanen <joel.mertanen@gmail.com>
2012-05-08 15:55:25 +0300

    added header, addresspicker zooms closer

--- server/public/javascripts/javascript.js
+++ server/public/javascripts/javascript.js
@@ -3,6 +3,15 @@ $(function() {
 		
 		var addresspickerMap = $( "#addresspicker_map" ).addresspicker({
 			regionBias: "fi",
+			mapOptions: {
+                  zoom: 13,
+/*                  tilt: 45, */
+/** Note! Give here the user's stored location! **/
+/*                  center: new google.maps.LatLng(48.85823517741984, 2.294511492065453), */
+                  mapTypeId: google.maps.MapTypeId.ROADMAP,
+                  streetViewControl: false,
+                  panControl: false
+            },
 		  elements: {
 		    map:      "#map",
 		    lat:      "#lat",
