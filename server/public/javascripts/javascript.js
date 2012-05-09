6d84c416e41c1f6b571fcc1d59f7b9764ca6df96
Joel Mertanen <joel.mertanen@gmail.com>
2012-05-09 13:36:49 +0300

    js cleanup

--- server/public/javascripts/javascript.js
+++ server/public/javascripts/javascript.js
@@ -1,13 +1,9 @@
 
 function initMap(lat, lon) {
-//	alert('lat: ' + lat + ', lon: ' + lon);
-
 		var addresspickerMap = $( "#addresspicker_map" ).addresspicker({
 			regionBias: "fi",
 			mapOptions: {
                   zoom: 13,
-/*                  tilt: 45, */
-/** Note! Give here the user's stored location! **/
                   center: new google.maps.LatLng(lat, lon),
                   mapTypeId: google.maps.MapTypeId.ROADMAP,
                   streetViewControl: false,
@@ -27,29 +23,6 @@ function initMap(lat, lon) {
 }
 
 $(function() {
-		
-/*		var addresspickerMap = $( "#addresspicker_map" ).addresspicker({
-			regionBias: "fi",
-			mapOptions: {
-                  zoom: 13,*/
-/*                  tilt: 45, */
-/** Note! Give here the user's stored location! **/
-/*                  center: new google.maps.LatLng(48.85823517741984, 2.294511492065453), */
-/*                  mapTypeId: google.maps.MapTypeId.ROADMAP,
-                  streetViewControl: false,
-                  panControl: false
-            },
-		  elements: {
-		    map:      "#map",
-		    lat:      "#lat",
-		    lng:      "#lng",
-		    locality: '#locality',
-		    country:  '#country'
-		  }
-		});
-		var gmarker = addresspickerMap.addresspicker( "marker");
-		gmarker.setVisible(true);
-		addresspickerMap.addresspicker( "updatePosition");*/
 	
 	/** Datepickers **/
 	var startAheadHours = 2;
