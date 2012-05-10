644d929e87ad767d9f7b719dcd4d0b452ba04168
Joel Mertanen <joel.mertanen@gmail.com>
2012-05-10 17:42:42 +0300

    the listing shows some fake data

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,10 +2,10 @@
 
 # --- !Ups
 
-insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url) values (  1,'Matti','Paananen',NOW(), '+358401234567', 'matti@expresscare.com', '/assets/images/babysitters/1.jpg');
-insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url) values (  2,'Pekka','Paananen',NOW(), '+358411234589', 'pekka@expresscare.com', '/assets/images/babysitters/2.jpg');
-insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url) values (  3,'Reino','Tulikoura',NOW(), '+358401234589', 'reino@expresscare.com', '/assets/images/babysitters/3.jpg');
-insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url) values (  4,'Äestäjä','Paananen',NOW(), '+358411234567', 'äestäjä@expresscare.com', '/assets/images/babysitters/4.jpg');
+insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url,basic_info) values (  1,'Matti','Paananen',NOW(), '+358401234567', 'matti@expresscare.com', '/assets/images/babysitters/1.jpg', 'I am a sports fan. Whether it is football, soccer, baseball or whateverball, I can handle it!');
+insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url,basic_info) values (  2,'Pekka','Paananen',NOW(), '+358411234589', 'pekka@expresscare.com', '/assets/images/babysitters/2.jpg', 'I am really fond of cooking (and) animals');
+insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url,basic_info) values (  3,'Reino','Tulikoura',NOW(), '+358401234589', 'reino@expresscare.com', '/assets/images/babysitters/3.jpg', 'I love animals and cooking.');
+insert into baby_sitter (id,first_name,last_name,birthday, phone, email, picture_url,basic_info) values (  4,'Äestäjä','Paananen',NOW(), '+358411234567', 'äestäjä@expresscare.com', '/assets/images/babysitters/4.jpg', 'I love cooking and stuff.');
 
 insert into baby_sitter_available (id,baby_sitter_id,start_time,end_time, requested, agreed) values (1, 1, '2012-05-01 12:12:12', '2012-05-10 12:12:12', false, false);
 insert into baby_sitter_available (id,baby_sitter_id,start_time,end_time, requested, agreed) values (2, 1, '2012-05-05 12:12:12', '2012-06-01 12:12:12', false, false);
